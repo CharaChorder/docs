17a53a99ef3229e3ce2635ca34ef9a9dced62868
CCMaverick <130024367+CCMaverick@users.noreply.github.com>
2025-04-09 11:37:03 -0500

    Master Forge Page
    Manual for the Master Forge.

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -135,6 +135,9 @@ html_context = {
     "conf_py_path": "/docs/",
     "source_suffix": source_suffix,
 }
+
+def setup(app):
+    app.add_css_file('custom.css')
 
 
 # -- Options for HTMLHelp output ------------------------------------------
