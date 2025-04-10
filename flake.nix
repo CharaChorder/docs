8a4731642acb3bcc4c21a51062411f3f672a8442
Thea Schöbl <dev@theaninova.de>
2025-04-10 18:50:05 +0200

    fix: images

--- flake.nix
+++ flake.nix
@@ -3,23 +3,29 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          python311
-          python311Packages.sphinx
-          python311Packages.sphinx-autobuild
-          python311Packages.sphinx-rtd-theme
-          python311Packages.sphinx-design
-          python311Packages.myst-parser
-        ];
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python311
+            python311Packages.sphinx
+            python311Packages.sphinx-autobuild
+            python311Packages.sphinx-rtd-theme
+            python311Packages.sphinx-design
+            python311Packages.sphinx-copybutton
+            python311Packages.myst-parser
+          ];
+        };
+      }
+    );
 }
