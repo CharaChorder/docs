8e822cb05e0e9688a8115911d8825d0aa1f04cfc
duianto <otnaiud@gmail.com>
2025-08-16 09:49:10 +0200

    Update conf.py, copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'CharaChorder Docs'
-copyright = '2024, CharaChorder'
+copyright = '2025, CharaChorder'
 author = 'CharaChorder'
 
 # The version info for the project you're documenting, acts as replacement for
